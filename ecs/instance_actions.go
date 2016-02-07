1b021d00473e2627b802d8ce6b845cc2b1020932
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2016-02-07 12:46:04 +0800

    move actions to one file

---
+++ ecs/instance_actions.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/caiguanhao/aliyun/vendor/cli"
+)
+
+type ActionResponse struct {
+	RequestId string `json:"RequestId"`
+}
+
+var REMOVE_INSTANCE cli.Command = cli.Command{
+	Name:    "remove-instance",
+	Aliases: []string{"remove", "d"},
+	Usage:   "remove an instance",
+	Action: func(c *cli.Context) {
+		Print(ECS_INSTANCE.RemoveInstanceById(c.Args().Get(0)))
+	},
+}
+
+var RESTART_INSTANCE cli.Command = cli.Command{
+	Name:    "restart-instance",
+	Aliases: []string{"restart", "r"},
+	Usage:   "restart an instance",
+	Action: func(c *cli.Context) {
+		Print(ECS_INSTANCE.RestartInstanceById(c.Args().Get(0)))
+	},
+}
+
+var START_INSTANCE cli.Command = cli.Command{
+	Name:    "start-instance",
+	Aliases: []string{"start", "s"},
+	Usage:   "start an instance",
+	Action: func(c *cli.Context) {
+		Print(ECS_INSTANCE.StartInstanceById(c.Args().Get(0)))
+	},
+}
+
+var STOP_INSTANCE cli.Command = cli.Command{
+	Name:    "stop-instance",
+	Aliases: []string{"stop", "k"},
+	Usage:   "stop an instance",
+	Action: func(c *cli.Context) {
+		Print(ECS_INSTANCE.StopInstanceById(c.Args().Get(0)))
+	},
+}
+
+func executeInstanceActionById(ecs *ECS, action, id string) (resp ActionResponse, _ error) {
+	return resp, ecs.Request(map[string]string{
+		"Action":     action,
+		"InstanceId": id,
+	}, &resp)
+}
+
+func (ecs *ECS) RemoveInstanceById(id string) (ActionResponse, error) {
+	return executeInstanceActionById(ecs, "DeleteInstance", id)
+}
+
+func (ecs *ECS) RestartInstanceById(id string) (ActionResponse, error) {
+	return executeInstanceActionById(ecs, "RebootInstance", id)
+}
+
+func (ecs *ECS) StartInstanceById(id string) (ActionResponse, error) {
+	return executeInstanceActionById(ecs, "StartInstance", id)
+}
+
+func (ecs *ECS) StopInstanceById(id string) (ActionResponse, error) {
+	return executeInstanceActionById(ecs, "StopInstance", id)
+}
+
+func (resp ActionResponse) Print() {
+	fmt.Println(resp.RequestId)
+}
+
+func (resp ActionResponse) PrintTable() {
+	fmt.Println(resp.RequestId)
+}
